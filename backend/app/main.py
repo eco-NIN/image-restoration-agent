8f2de645866032f8cef7deec9abf986565804886
eco-NIN <2321987772@qq.com>
2026-03-06 17:49:07 +0800

    initial project

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+# @Author  :eco
+# @Date    :2026/3/6 17:30
+# @Function:
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Image Restoration Agent API"}
